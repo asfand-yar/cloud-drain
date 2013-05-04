539cc4e70f34d1af1ffd75d1ec5b68d17532644c
asfand-yar <muhammad_asfand@hotmail.com>
2013-05-04 17:23:29 +0600

    Create reg.php

---
+++ reg.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+	<title>Cloud Drain</title>
+	<link rel="shortcut icon" type="image/x-icon" href="css/images/favicon.ico" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+	<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="all" />
+	
+	<meta http-equiv="content-type" content="text/html;charset=UTF-8" />    
+	<meta name="keywords" content=""></meta>
+	<meta name="description" content="Free form designs from CSS Globe"></meta>
+	<meta http-equiv="imagetoolbar" content="no" />
+	<link rel="stylesheet" href="css/screen.css" media="screen" />
+	
+	<script src="js/jquery-1.7.2.min.js" type="text/javascript"></script>
+	<!--[if lt IE 9]>
+		<script src="js/modernizr.custom.js"></script>
+	<![endif]-->
+	<script src="js/jquery.flexslider-min.js" type="text/javascript"></script>
+	<script src="js/functions.js" type="text/javascript"></script>
+</head>
+<body>
+	<!-- wrapper -->
+	<div id="wrapper2">
+		<!-- header -->
+		<header class="header">
+			<div class="shell">
+				<h1 id="logo"><a href="#">Cloud Drain</a></h1>
+				
+				<!-- navigation -->
+				<nav id="navigation">
+					<ul>
+						<li class="active">
+							<a href="home.html">Home</a>
+						</li>
+						<li>
+							<a href="login.html">Sign In</a>
+						</li>
+						<li>
+							<a href="#">Contact Us</a>
+						</li>
+					
+					</ul>
+					<div class="cl">&nbsp;</div>
+				</nav>
+				<!-- end of navigation -->
+			</div>
+		</header>
+		<!-- end of header -->
+		<div id="c">
+		<form id="form2" action="regpro.php" method="POST">
+		<center>
+		<table>
+		<tr>
+			<td><label for='name' >Name: </label></td>
+			<td><input type='text' name='name' id='name' maxlength="20" /></td>
+		</tr>
+
+		<tr>
+			<td><label for='email' >Email: </label></td>
+			<td><input type='text' name='email' id='email' maxlength="25" /></td>
+		</tr>
+		<tr>
+			<td><label for='username' >Username: </label></td>
+			<td><input type='text' name='username' id='username' maxlength="15" /></td>
+		</tr>
+		<tr>
+			<td><label for='password' >Password: </label></td>
+			<td><input type='text' name='password' id='password' maxlength="15" /></td>
+		</tr>
+		<tr>
+			<td colspan="2"><center><input class="register" type='submit' name='Submit' value='Register' /><center>
+			<!--<input type="submit" value="" class="register" />--></td>
+		</tr>
+		</table>
+		</center>
+		</form>
+		</div>
+		
+		<!-- <div id="footer-push"></div> -->
+	</div>
+	<!-- wrapper -->
+
+	<!-- footer -->
+	<div id="footer">
+		<div class="shell">
+			<!-- footer-cols -->
+			<section class="footer-cols">
+				<div class="col">
+					<h3>MORE <strong>ABOUT US</strong></h3>
+					<p>-----</p>
+				</div>
+				<div class="col">
+					<h3>Be <strong>Informed</strong></h3>
+					<p>Subscribe for our newsletters and latest news.</p>
+					<form method="post">
+						<input type="text" class="field" value="Your Email Here" title="Your Email Here" />
+						<input type="submit" class="submit-btn" value="Submit" />
+					</form>
+				</div>
+				<div class="col contact">
+					<h3>CONTACT <strong>US</strong></h3>
+					<h4>+92 332 5099104</h4>
+					<a href="#">clouddrain@gmail.com</a>
+				</div>
+				<div class="cl">&nbsp;</div>
+			</section>
+			<!-- end of footer-cols -->
+			
+			<div class="cl">&nbsp;</div>
+		</div>	
+	</div>
+	<!-- end of footer -->
+</body>
+</html>
